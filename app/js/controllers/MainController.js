2612427dfdfcd010ca8cce6f1feb2d034430210f
Nick Leach <nickleach22@gmail.com>
2015-12-07 15:30:09 -0500

    added sidebar

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,6 +1,35 @@
-function MainController($scope){
+function MainController($scope, $timeout, $mdSidenav, $log, $mdUtil, $state){
+
+  function buildToggler(navID) {
+    let debounceFn =  $mdUtil.debounce(() => {
+          $mdSidenav(navID).toggle();
+      },300);
+    return debounceFn;
+  }
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+    // nav toggles
+  $scope.toggleLeft = buildToggler('left');
+  $scope.toggleRight = buildToggler('right');
+  $scope.navigateTo = function(state){
+    $state.go(state);
+  };
+}
+function RightCtrl($scope, $mdSidenav){
+
+  $scope.close = function () {
+    $mdSidenav('right').close();
+  };
 
 }
+function LeftCtrl($scope, $mdSidenav){
+  $scope.close = function () {
+    $mdSidenav('left').close();
+  };
+}
 export {
-  MainController
+  MainController,
+  RightCtrl,
+  LeftCtrl
 };
--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 import { config } from './config';
-import { MainController } from './controllers/MainController';
+import { MainController, LeftCtrl, RightCtrl } from './controllers/MainController';
 import { MediaController } from './controllers/MediaController';
 
 angular.module('app', ['ui.router', 'ngMaterial'])
 .config(config)
 .controller('MainController', MainController)
+.controller('LeftCtrl', LeftCtrl)
+.controller('RightCtrl', RightCtrl)
 .controller('MediaController', MediaController);
