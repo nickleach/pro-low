dbcfb7b1a0709a42f18ec582228a8afe02a0185a
Nick Leach <nickleach22@gmail.com>
2016-01-14 18:07:31 -0500

    added basic testimonial stuff

--- app/js/controllers/TestimonialController.js
+++ app/js/controllers/TestimonialController.js
@@ -1,12 +1,14 @@
-export default function TestimonialController($scope, TestimonialService){
+export default function TestimonialController($scope, TestimonialService, $log){
   $scope.loadingTestimonials = true;
 
   TestimonialService.getTestimonials()
     .success((data) =>{
       $scope.loadingTestimonials = false;
       $scope.testimonials = data;
+      $log.debug($scope.testimonials);
     })
     .error((data) =>{
+      $scope.loadingTestimonials = false;
       $scope.errorMessage = "We're sorry we could not load testimonials at this time. Please try again later.";
     });
 
